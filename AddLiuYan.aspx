16b7595798480000591829ff36310a891aa00652
wangzixu111 <2395293586@qq.com>
2018-02-19 13:57:22 +0800

    提交文件

---
+++ AddLiuYan.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="AddLiuYan.aspx.cs"  ValidateRequest="false" EnableEventValidation="false"  Inherits="AddLiuYan" Title="Untitled Page" %>
+ 
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table style="width: 100%">
+        <tr>
+            <td align="left" height="40" style="  text-align: left">
+                标题：<asp:TextBox ID="tb_title" runat="server" MaxLength="100" Rows="1" Width="493px"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td style="color: #999999;   text-align: center">
+                内容</td>
+        </tr>
+        <tr>
+            <td style="height: 161px" valign="top">
+                &nbsp;<asp:TextBox ID="TextBox1" runat="server" Height="164px" MaxLength="100" Rows="1"
+                    TextMode="MultiLine" Width="100%"></asp:TextBox></td>
+        </tr>
+        <tr>
+            <td align="center" valign="top">
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="发布" />&nbsp;</td>
+        </tr>
+    </table>
+</asp:Content>
+
---
+++ AddLiuYan.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+public partial class AddLiuYan : System.Web.UI.Page
+{
+    SqlHelper data = new SqlHelper();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.Title = "休渔期网站设计";
+        if (!IsPostBack)
+        {
+            if (Session["UserName"] == null)
+            {
+                Alert.AlertAndRedirect("对不起您还没有登录", "Default.aspx");
+            }
+            else
+            {
+
+ 
+
+
+              
+            }
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        SqlHelper data = new SqlHelper();
+        data.RunSql("insert into LiuYan(LiuYan,AddUser,Content,AddUserId)values('" + tb_title.Text + "','" + Session["UserName"].ToString() + "','" + TextBox1.Text + "','" + Session["UserId"].ToString() + "')");
+        Alert js = new Alert();
+        js.Alertjs("发布成功");
+        Response.Redirect("LiuYan.aspx");
+    }
+   
+}
